3d6cb0b37e0b8c272ca1e52184cb83ea795fcfa4
namank26-qa <namankumarqa26@gmail.com>
2025-02-18 22:08:11 +0530

    Adding demo code for understading stash

--- src/main/java/com/qa/opencart/pages/DemoStash.java
+++ src/main/java/com/qa/opencart/pages/DemoStash.java
@@ -3,11 +3,12 @@ package com.qa.opencart.pages;
 public class DemoStash {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 
 		System.out.println("Demo stash from workspace");
 
 
 
-	}
+}
 
 }
