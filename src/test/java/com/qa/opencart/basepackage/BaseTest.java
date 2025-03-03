05b773889685d5dacd631a19701ba3c1fec65afe
namank26-qa <namankumarqa26@gmail.com>
2025-03-03 21:14:04 +0530

    added testname parameter in basetest class and incresed thread count in regression.xml file

--- src/test/java/com/qa/opencart/basepackage/BaseTest.java
+++ src/test/java/com/qa/opencart/basepackage/BaseTest.java
@@ -38,13 +38,14 @@ public class BaseTest {
 
 	@Parameters({"browser","browserversion","testname"}) //browser value coming from xml file
 	@BeforeTest
-	public void setup(String browserName, String browserVersion) { //parameter value
+	public void setup(String browserName, String browserVersion, String testname) { //parameter value
 		df = new DriverFactory();
 		prop = df.initProp(); //properties coming from config.properties file
 		
 		if(browserName != null) { 
 			prop.setProperty("browser", browserName); //set browser property from xml file
 			prop.setProperty("browserversion", browserVersion);
+			prop.setProperty("testname", testname);
 		}
 		driver = df.initDriver(prop);
 
